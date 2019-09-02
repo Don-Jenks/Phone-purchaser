2fdfbedafa9f0df57dcedd0510b1541c22549a39
Donald Jenkins <53826081+Zer0-Drive@users.noreply.github.com>
2019-09-02 12:50:26 -0400

    Add files via upload
    There are specific notes inside the .js files for your perusal.

---
+++ phone purchase redux.js
@@ -0,0 +1,30 @@
+﻿
+//constants
+const tax = 0.08;
+const phonePrice = 99.99;
+const accessPrice = 9.99;
+const mentalSpending = 300.00;
+
+//variables
+let phone = 0;
+let accessory = 0;
+let amount = 0.00;
+let balance = 500.00;
+
+//while loop to add to your amount, there's a problem where it will go over your balance though...
+while(amount < balance) {
+amount = amount + phonePrice;
+phone++;
+document.write("You bought a phone!<br>");
+if(amount < mentalSpending) {
+amount = amount + accessPrice;
+accessory++;
+document.write("You bought an accessory!<br>");
+} else {
+document.write("You can no longer purchase any more accessories.<br>"); // I need to fix this as it will keep printing ad nauseum, maybe just take it out all together
+}
+total = (amount + (amount * tax)).toFixed(2); //This may need to be moved higher up in the function? Testing needed.
+}
+document.write("You can no longer purchase any more phones.<br>");
+document.write("Your total is, $" + total + " plus tax<br>");
+document.write("You bought " + phone + " phones and " + accessory + " accessories.");
---
+++ phone purchase.js
@@ -0,0 +1,33 @@
+﻿
+/* This is the original version of this challenge that I came up with,
+the problem is really that it would print the wrong total amount,
+I solved it with the next version. */
+
+//variables
+let phone = 0;
+let accessory = 0;
+let account = 500.00;
+const tax = 0.08;
+const phonePrice = 99.99;
+const accessPrice = 19.99;
+const mentalSpending = 150.00;
+
+
+//while loop to calculate total
+while(account > phonePrice) {
+account = account - phonePrice;
+phone++;
+document.write("You bought a phone!<br>");
+if(account > mentalSpending) {
+account = account - accessPrice;
+accessory++;
+document.write("You bought an accessory!<br>");
+} else {
+document.write("You can no longer purchase any more accessories.<br>");
+}
+}
+document.write("You can no longer purchase any more phones.<br>");
+let total = (account * tax).toFixed(2);
+document.write("Your total is, $" + total + " plus tax");
+document.write("You bought " + phone + " phones and " + accessory + " accessories.");
+
